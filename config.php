6d2643e3006103d3e42d3bef30d997b0ac47e95d
brittaney-gonzalez <38170052+brittaney-gonzalez@users.noreply.github.com>
2019-02-24 18:35:40 -0500

    Module 3
    Includes html for additional pages, php pages for database, and associated images.

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'nPrMfCLFHT8q'); // You need to add in your Bitnami password here 
+define('DB_NAME', 'weddingdb'); // your database name is demo???? 
+ 
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+?>
